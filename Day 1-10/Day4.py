b0574670300e515468a83882d1d1c5f62efcb85f
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2023-12-04 23:40:52 +0530

    Create Day4.py

---
+++ Day 1-10/Day4.py
@@ -0,0 +1,22 @@
+#https://www.geeksforgeeks.org/problems/sum-string3151/1
+
+class Solution {
+public:
+    std::string largestGoodInteger(string num) {
+        char com;
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < num.length() - 2; i++) {
+            if (num[i] == num[i + 1] && num[i + 1] == num[i + 2]) {
+                if (com <= num[i]) {
+                    left = i;
+                    right = i + 2;
+                    com = num[i];
+                }
+            }
+        }
+        if(com)
+            return num.substr(left, 3);
+        return "";
+    }
+};

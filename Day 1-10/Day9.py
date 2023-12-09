9d5c772dfc786221a2b889ce295ee875bd79f9cb
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2023-12-09 23:23:15 +0530

    Create Day9.py

---
+++ Day 1-10/Day9.py
@@ -0,0 +1,25 @@
+#https://www.geeksforgeeks.org/problems/smith-number4132/1
+
+class Solution:
+    def smithNum(self, n):
+        # code here 
+        def cd(n):
+            ans = 0
+            while n > 0:
+                ans += (n % 10)
+                n //= 10
+            return ans
+        digitsum = cd(n)
+        primefact = 0
+        temp = n
+        div = 2
+        while temp > 0 and div < n:
+            while temp % div == 0:
+                primefact += cd(div)
+                temp //= div
+            div += 1
+        if temp > 1:
+            return 0
+        if digitsum == primefact:
+            return 1
+        return 0

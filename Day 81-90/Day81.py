c1d793a3c9a23265bc231689cd14033673968bb6
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-02-19 22:29:03 +0530

    Create Day81.py

---
+++ Day 81-90/Day81.py
@@ -0,0 +1,24 @@
+# https://www.geeksforgeeks.org/problems/game-with-string4100/1
+
+
+from collections import Counter
+import heapq
+
+class Solution:
+    def minValue(self, s, k):
+        counter = Counter(s)
+        
+        counts = counter.values()
+        counts = [-x for x in counts]
+        heapq.heapify(counts)
+        
+        while k > 0:
+            curItem = -heapq.heappop(counts)
+            curItem -= 1
+            k -= 1
+            heapq.heappush(counts, -curItem)
+        
+        res = 0
+        for count in counts:
+            res += (-count) ** 2
+        return res

b3948264c003ccc29e6b42cc6d63cb016eef72d0
Aditya Singh <124086655+adysingh5711@users.noreply.github.com>
2024-02-25 22:16:19 +0530

    Create Day87.py

---
+++ Day 81-90/Day87.py
@@ -0,0 +1,18 @@
+# https://www.geeksforgeeks.org/problems/reach-a-given-score-1587115621/1
+
+
+class Solution:
+    def count(self, n: int) -> int:
+        #your code here
+        dp = [0] * (n + 1)
+
+        # There is one way to achieve a score of 0 (no moves)
+        dp[0] = 1
+
+        # Iterate through each move (3, 5, 10)
+        for move in [3, 5, 10]:
+            # Update dp array for each score from move to n
+            for score in range(move, n + 1):
+                dp[score] += dp[score - move]
+
+        return dp[n]
